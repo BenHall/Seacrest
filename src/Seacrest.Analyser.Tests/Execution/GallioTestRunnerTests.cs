1eb8e11bb47965aff65a67a196595a462456b955
BenHall <ben@benhall.me.uk>
2010-07-18 14:17:41 +0100

    Moved runner to correct folder

--- src/Seacrest.Analyser.Tests/Execution/GallioTestRunnerTests.cs
+++ src/Seacrest.Analyser.Tests/Execution/GallioTestRunnerTests.cs
@@ -1,9 +1,6 @@
-using System;
-using System.IO;
-using System.Linq;
-using System.Text;
 using NUnit.Framework;
 using System.Collections.Generic;
+using Seacrest.Analyser.Execution;
 using Seacrest.Analyser.Parsers.TestExplorer;
 
 namespace Seacrest.Analyser.Tests.Execution
@@ -39,36 +36,6 @@ namespace Seacrest.Analyser.Tests.Execution
             string argument = runner.CreateArguments(testsToExecute);
 
             Assert.IsTrue(argument.StartsWith("Path\\Assembly1.dll"));
-        }
-    }
-
-    public class GallioTestRunner
-    {
-        public string CreateArguments(List<Test> testsToExecute)
-        {
-            StringBuilder builder = new StringBuilder();
-            var firstTest = testsToExecute.First();
-            if (firstTest != null)
-            {
-                List<string> filters = BuildFilterList(testsToExecute);
-
-                builder.Append(Path.Combine(firstTest.PathToAssembly, firstTest.AssemblyName + ".dll"));
-                builder.Append(" ");
-                builder.Append(String.Join(" AND ", filters.ToArray()));
-                builder.Append(" /np /v:Quiet /no-echo-results");
-            }
-
-            return builder.ToString();
-        }
-
-        private List<string> BuildFilterList(List<Test> testsToExecute)
-        {
-            List<string> filters = new List<string>();
-            foreach (var test in testsToExecute)
-            {
-                filters.Add(string.Format("(Type:{0} AND Member:{1})", test.ClassName, test.MethodName));
-            }
-            return filters;
         }
     }
 }
---
+++ src/Seacrest.Analyser/Execution/GallioTestRunner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Seacrest.Analyser.Parsers.TestExplorer;
+
+namespace Seacrest.Analyser.Execution
+{
+    public class GallioTestRunner
+    {
+        public string CreateArguments(List<Test> testsToExecute)
+        {
+            StringBuilder builder = new StringBuilder();
+            var firstTest = testsToExecute.First();
+            if (firstTest != null)
+            {
+                List<string> filters = BuildFilterList(testsToExecute);
+
+                builder.Append(Path.Combine(firstTest.PathToAssembly, firstTest.AssemblyName + ".dll"));
+                builder.Append(" ");
+                builder.Append(String.Join(" AND ", filters.ToArray()));
+                builder.Append(" /np /v:Quiet /no-echo-results");
+            }
+
+            return builder.ToString();
+        }
+
+        private List<string> BuildFilterList(List<Test> testsToExecute)
+        {
+            List<string> filters = new List<string>();
+            foreach (var test in testsToExecute)
+            {
+                filters.Add(string.Format("(Type:{0} AND Member:{1})", test.ClassName, test.MethodName));
+            }
+            return filters;
+        }
+    }
+}
