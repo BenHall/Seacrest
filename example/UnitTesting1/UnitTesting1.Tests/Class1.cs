f2286e736cd6a02c833712bd2b98e23615cdfebe
BenHall <ben@benhall.me.uk>
2010-07-18 22:52:19 +0100

    Example for end-to-end testing

---
+++ example/UnitTesting1/UnitTesting1.Tests/Class1.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace UnitTesting1.Tests
+{
+    [TestFixture]
+    public class OneMethodClassTests
+    {
+        [Test]
+        public void Test_Example1_With_One_Method_Call()
+        {
+            OneMethodClass oneMethodClass = new OneMethodClass();
+            string v = oneMethodClass.Method1();
+            Assert.That(v, Is.EqualTo("Method1 String"));
+        }
+
+        [Test]
+        public void Test_Example2_With_One_Method_Call()
+        {
+            OneMethodClass oneMethodClass = new OneMethodClass();
+            string v = oneMethodClass.Method2();
+            Assert.That(v, Is.EqualTo("Method2 String"));
+        }
+    }
+}
---
+++ example/UnitTesting1/UnitTesting1/OneMethodClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTesting1
+{
+    public class OneMethodClass
+    {
+        public string Method1()
+        {
+            throw new NotImplementedException();
+            return "Method1 String   ";
+        }
+
+        public string Method2()
+        {
+            return "Method2 String";
+        }
+    }
+}
